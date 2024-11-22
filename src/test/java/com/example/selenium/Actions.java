0a4b75d3151be1eed4b78cbee8729603ac4cf13e
Sive Joba <sivejoba@gmail.com>
2024-11-22 10:43:28 +0200

    first-commit

---
+++ src/test/java/com/example/selenium/Actions.java
@@ -0,0 +1,167 @@
+package com.example.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.UUID;
+
+public class Actions extends Elements {
+
+    private WebDriver driver;
+
+    // Constructor
+    public Actions(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public static String generateRandomEmail() {
+        String uniqueID = UUID.randomUUID().toString(); // Generates a unique ID
+        return "user" + uniqueID + "@example.com"; // Creates a random email
+    }
+
+    // Register User
+
+    public void registerUser() {
+        driver.findElement(Elements.Register_Button).click();
+    }
+
+    public void genderButton() {
+        driver.findElement(Elements.Gender_Radiobutton).click();
+    }
+
+    public void enterFirstname(String firstname) {
+        driver.findElement(Elements.FirstName).sendKeys(firstname);
+    }
+
+    public void enterLastname(String lastname) {
+        driver.findElement(Elements.LastName).sendKeys(lastname);
+    }
+
+    public void enterEmail(String email) {
+        driver.findElement(Elements.Email).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(Elements.Password_Field).sendKeys(password);
+    }
+
+    public void confirmPassword(String confirm_password) {
+        driver.findElement(Elements.Confirm_password).sendKeys(confirm_password);
+    }
+
+    public void secondResister() {
+        driver.findElement(Elements.Second_Register).click();
+    }
+
+    public void logoutMenu() {
+        driver.findElement(Elements.Logout_Button).click();
+    }
+
+
+    //Login User
+
+    public void loginMenu() {
+        driver.findElement(Elements.Login_Button).click();
+    }
+
+    public void login_User(String login) {
+        driver.findElement(Elements.Username_Text).sendKeys(login);
+    }
+
+    public void user_Pass(String pass) {
+        driver.findElement(Elements.Password_Text).sendKeys(pass);
+    }
+
+    public void login_Inside() {
+        driver.findElement(Elements.Login_inside).click();
+    }
+
+    public void computers_tab() {
+        driver.findElement(Elements.Computers_Tab).click();
+    }
+
+    public void desktops() {
+        driver.findElement(Elements.Desktops).click();
+    }
+
+    public void dropdown() {
+        driver.findElement(Elements.Order_By).click();
+    }
+
+    public void AllOptions() {
+        driver.findElement(Elements.Position).click();
+        driver.findElement(Elements.AtoZ).click();
+        driver.findElement(Elements.ZtoA).click();
+        driver.findElement(Elements.HighToLow).click();
+        driver.findElement(Elements.LowToHigh).click();
+        driver.findElement(Elements.CreatedOn).click();
+
+    }
+public void simpleComputer(){
+    driver.findElement(Elements.SimpleComputer).click();
+    driver.findElement(Elements.Processor).click();
+    driver.findElement(Elements.AddSimpleComputer).click();
+    driver.findElement(Elements.DesktopsAgain).click();
+
+}
+
+public void buildYourOwn(){
+    driver.findElement(Elements.BuildOwnComputer).click();
+    driver.findElement(Elements.HDD).click();
+    driver.findElement(Elements.AddOwnCom).click();
+    driver.findElement(Elements.DesktopsAgain2).click();
+
+}
+
+
+public void buildOwnCheap(){
+    driver.findElement(Elements.BuildOwnCheap).click();
+    driver.findElement(Elements.AddOwnCheap).click();
+    driver.findElement(Elements.DesktopsAgain2).click();
+}
+
+public void buildOwnExpensive(){
+    driver.findElement(Elements.BuildOwnExpensive).click();
+    driver.findElement(Elements.AddOwnExpensive).click();
+    driver.findElement(Elements.DesktopsAgain2).click();
+    driver.findElement(Elements.Cart).click();
+}
+
+
+    public double getCartTotalPrice() {
+        // Locate the total price element on the cart page
+        WebElement totalPriceElement = driver.findElement(By.className("cart-total-left"));  // Update with actual locator
+
+        // Extract the price as a string
+        String totalPriceText = totalPriceElement.getText();
+
+        // Remove any currency symbols or extra characters, then convert to double
+        totalPriceText = totalPriceText.replaceAll("[^\\d.]", "");  // This will remove any non-numeric characters (e.g., $)
+        return Double.parseDouble(totalPriceText);  // Convert the cleaned string to a double
+    }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/com/example/selenium/Elements.java
@@ -0,0 +1,73 @@
+package com.example.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Elements {
+
+
+    //Register
+    public static final By Register_Button = By.className("ico-register");
+    public static final By Gender_Radiobutton = By.id("gender-female");
+    public static final By FirstName = By.id("FirstName");
+    public static final By LastName = By.id("LastName");
+    public static final By Email = By.id("Email");
+    public static final By Password_Field = By.id("Password");
+    public static final By Confirm_password = By.id("ConfirmPassword");
+    public static final By Second_Register = By.id("register-button");
+    public static final By Logout_Button = By.className("ico-logout");
+
+
+
+   //Login
+   public static final By Login_Button = By.className("ico-login");
+   public static final By Username_Text = By.name("Email");
+   public static final By Password_Text = By.name("Password");
+   public static final By Login_inside = By.className("button-1");
+   public static final By Computers_Tab = By.linkText("Computers");
+   public static final By Desktops = By.linkText("Desktops");
+
+   //OrderBy
+   public static final By Order_By = By.id("products-orderby");
+   public static final By Position = By.xpath("//option[text()='Position']");
+   public static final By AtoZ = By.xpath("//*[@id='products-orderby']/option[2]");
+   public static final By ZtoA = By.xpath("//*[@id='products-orderby']/option[3]");
+   public static final By HighToLow = By.xpath("//*[@id='products-orderby']/option[4]");
+   public static final By LowToHigh = By.xpath("//*[@id='products-orderby']/option[5]");
+   public static final By CreatedOn = By.xpath("//*[@id='products-orderby']/option[6]");
+
+   //Add To Cart
+   public static final By SimpleComputer = By.linkText("Simple Computer");
+   public static final By Processor = By.id("product_attribute_75_5_31_96");
+   public static final By AddSimpleComputer = By.id("add-to-cart-button-75");
+   public static final By DesktopsAgain = By.linkText("Desktops");
+   public static final By BuildOwnComputer = By.linkText("Build your own computer");
+   public static final By HDD = By.id("product_attribute_16_3_6_18");
+   public static final By AddOwnCom = By.id("add-to-cart-button-16");
+    public static final By DesktopsAgain2 = By.linkText("Desktops");
+    public static final By BuildOwnCheap = By.linkText("Build your own cheap computer");
+    public static final By AddOwnCheap = By.id("add-to-cart-button-72");
+    public static final By BuildOwnExpensive = By.linkText("Build your own expensive computer");
+    public static final By AddOwnExpensive = By.id("add-to-cart-button-74");
+    public static final By Cart = By.linkText("Shopping cart");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
